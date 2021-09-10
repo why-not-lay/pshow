4349d28ac5885270d2cb3b116a04915a1faea380
losorin <luoyihong4@163.com>
2021-09-10 20:04:19 +0800

    实现验证码生成

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const Koa = require('koa');
-const Router = require('koa-router');
+const Koa = require("koa");
+const Router = require("koa-router");
+const Logger = require("koa-logger");
+const {createRandomNumber} = require("./lib/random");
 const app = new Koa();
 const router = new Router();
-router.get('/', (ctx, next) => {
 
+router.get("/authenticate", (ctx, next) => {
+  ctx.body = {
+    status: 'success',
+    code: createRandomNumber(10000, 100000),
+  };
 });
 app
+  .use(Logger())
   .use(router.routes())
   .use(router.allowedMethods());
-app.listen(9090);
+const server = app.listen(9090);
+
+process.on("SIGTERM", ()=>{
+  server.close(() => {
+    console.log("服务关闭");
+  });
+})
